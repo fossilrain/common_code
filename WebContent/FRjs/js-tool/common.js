5e6dbf9542fd25a1ecf158dd9da243adedb49607
fossilrain@org <1024254883@qq.com>
2016-12-27 09:43:32 +0800

    common.js + 四舍六入函数

--- WebContent/FRjs/js-tool/common.js
+++ WebContent/FRjs/js-tool/common.js
@@ -51,7 +51,26 @@ Object.prototype._clone=function(){
 	}
 	return obj;
 }
-
+/**
+ * 无需修约的将直接返回 arg 需要四舍六入五留双的数 count 需要保留的小数点位数
+ * toFixed()方法也是四舍六入，但其留双时时偶数进位，哪一个正确？
+ */
+function numFormat(arg, count) {
+	if (!isNaN(arg) && count > 0) {
+		var argStr = Number(arg).toString();
+		if (new RegExp("^\\d+\\.\\d{" + (count + 1) + ",}$").test(argStr)) {// 需要修约正则
+			var tarArg = Number(argStr.substr(0, argStr.length - (argStr.split(".")[1].length - count)));
+			var m = Math.pow(10, count);
+			if (new RegExp("^\\d+\\.\\d{" + count + "}[6-9]\\d*$").test(argStr) || // 6入
+				new RegExp("^\\d+\\.\\d{" + (count - 1) + "}[1|3|5|7|9][5]$").test(argStr) || // 5前为奇数
+				new RegExp("^\\d+\\.\\d{" + count + "}[5]\\d+$").test(argStr)) {// 5后有大于0的数
+				return (tarArg * m + 1) / m;
+			}
+			return (tarArg * m) / m;
+		}
+	}
+	return arg;
+}
 
 
 
